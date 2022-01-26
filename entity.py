9701085539fc9a831d62ee696353c06ebc32f0b3
James Harmon <jamesharmon@Jamess-Mac-mini.local>
2022-01-26 10:41:34 -0500

    Initial commit

---
+++ entity.py
@@ -0,0 +1,103 @@
+from __future__ import annotations
+
+import copy
+from typing import Optional, Tuple, Type, TypeVar, TYPE_CHECKING
+
+from render_order import RenderOrder
+
+if TYPE_CHECKING:
+    from components.ai import BaseAI
+    from components.fighter import Fighter
+    from game_map import GameMap
+
+T = TypeVar("T", bound="Entity")
+
+
+class Entity:
+    """
+    A generic object to represent players, enemies, items etc..
+    """
+    parent: GameMap
+
+    def __init__(
+            self,
+            parent: Optional[GameMap] = None,
+            x: int = 0,
+            y: int = 0,
+            char: str = "?",
+            color: Tuple[int, int, int] = (255, 255, 255),
+            name: str = "<unknown>",
+            blocks_movement: bool = False,
+            render_order: RenderOrder = RenderOrder.CORPSE
+    ):
+        self.x = x
+        self.y = y
+        self.char = char
+        self.color = color
+        self.name = name
+        self.blocks_movement = blocks_movement
+        self.render_order = render_order
+        if parent:
+            # if gamemap is not provided now then it will be set later
+            self.parent = parent
+            parent.entities.add(self)
+
+    @property
+    def gamemap(self) -> GameMap:
+        return self.parent.gamemap
+
+    def spawn(self: T, gamemap: GameMap, x: int, y: int) -> T:
+        """ Spawns a copy of this instance at given location."""
+        clone = copy.deepcopy(self)
+        clone.x = x
+        clone.y = y
+        clone.parent = gamemap
+        gamemap.entities.add(clone)
+        return clone
+
+    def place(self, x: int, y: int, gamemap: Optional[GameMap] = None) -> None:
+        self.x = x
+        self.y = y
+        if gamemap:
+            if hasattr(self, "parent"):
+                if self.parent is self.gamemap:
+                    self.gamemap.entities.remove(self)
+            self.parent = gamemap
+            gamemap.entities.add(self)
+
+    def move(self, dx: int, dy: int) -> None:
+        # Move the entity by a given amount
+        self.x += dx
+        self.y += dy
+
+
+class Actor(Entity):
+    def __init__(
+            self,
+            *,
+            x: int = 0,
+            y: int = 0,
+            char: str = "?",
+            color: Tuple[int, int, int] = (255, 255, 255),
+            name: str = "<unknown>",
+            ai_cls: Type[BaseAI],
+            fighter: Fighter
+    ):
+        super().__init__(
+            x=x,
+            y=y,
+            char=char,
+            color=color,
+            name=name,
+            blocks_movement=True,
+            render_order=RenderOrder.ACTOR,
+        )
+        self.ai: Optional[BaseAI] = ai_cls(self)
+
+        self.fighter = fighter
+        self.fighter.parent = self
+
+    @property
+    def is_alive(self) -> bool:
+        # returns a true
+        return bool(self.ai)
